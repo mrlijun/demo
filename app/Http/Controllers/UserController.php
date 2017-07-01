d88b288e8a6e0d8e845d41750203680773d831d3
leejohn <leejohn163@163.com>
2017-07-01 15:02:29 +0800

    完成用户注册

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,10 +7,35 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Models\User;
+
 class UserController extends Controller
 {
     public function create()
     {
         return view('user.create');
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return view('user.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success','恭喜你，注册成功了，继续敲吧！！！');
+        return redirect()->route('user.show',[$user]);
     }
 }
--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -9,6 +9,7 @@
 @include('layout._header')
     <div class="container">
         <div class="col-md-offset-1 col-md-10">
+            @include('shared.messages')
             @yield('content')
             @include('layout._footer')
         </div>
